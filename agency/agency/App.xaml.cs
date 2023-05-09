f48527d4cdeb454b408f9c8eb9c572ec801f68fc
gorchakovbtw <133045067+gorchakovbtw@users.noreply.github.com>
2023-05-09 23:34:02 +0300

    Add files via upload

---
+++ agency/agency/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace agency
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public static Entities.ISP31Entities Context
+        { get; } = new Entities.ISP31Entities();
+
+        public static Entities.User Currentuser = null;
+    }
+}
